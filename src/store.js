010d7a659164a91cd70923d4bf79bc9d12457d86
Huw Roberts MBP <huwroberts@Huws-MBP.lan>
2018-11-27 16:49:41 +0000

    Changes to store data structure

--- src/store.js
+++ src/store.js
@@ -6,12 +6,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    caseStudies: {},
-    caseStudy: {}
+    caseStudies: [],
+    caseStudy: {},
+    aboutMe: ''
   },
   mutations: {
     SET_CASE_STUDIES(state, caseStudies) {
       state.caseStudies = caseStudies
+    },
+    SET_CASE_STUDY(state, caseStudy) {
+      state.caseStudy = caseStudy
+    },
+    SET_ABOUT_ME(state, aboutMe) {
+      state.aboutMe = aboutMe
     }
   },
   actions: {
@@ -21,8 +28,43 @@ export default new Vuex.Store({
           commit('SET_CASE_STUDIES', response.data)
         })
         .catch(error => {
-          console.log('There was an error:', error.response)
+          console.log('There was an error: ', error.response)
         })
+    },
+    fetchCaseStudy({ commit, getters }, titleslug) {
+      // Check if it exists in caseStudies
+      /*
+      var casestudy = getters.getCaseStudyByID(titleslug)
+
+      if (casestudy) {
+        commit('SET_CASE_STUDY', casestudy)
+      } else {*/
+      return ContentService.getCaseStudy(titleslug)
+        .then(response => {
+          commit('SET_CASE_STUDY', response.data)
+          return response.data
+        })
+        .catch(error => {
+          console.log('There was an error: ', error.response)
+        })
+      //}
+    },
+    fetchAboutMe({ commit }) {
+      return ContentService.getAboutMe()
+        .then(response => {
+          commit('SET_ABOUT_ME', response.data)
+          return response.data
+        })
+        .catch(error => {
+          console.log('There was an error: ', error.response)
+        })
+    }
+  },
+  getters: {
+    getCaseStudyByID: state => titleslug => {
+      return state.caseStudies.find(
+        casestudy => casestudy.title_slug === titleslug
+      )
     }
   }
 })
