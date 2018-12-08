d1a2e671cf84cf87c2990c247636de1be875f6be
Huw Roberts MBP <huwroberts@Huws-MBP.lan>
2018-12-08 21:59:11 +0000

    Added pop on scroll animation to CaseStudyCard

--- src/components/CaseStudyCard.vue
+++ src/components/CaseStudyCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fit-height featured-work--item item">
+  <div :class="{hello: popIt}" class="fit-height featured-work--item item">
     <div class="pad">
       <div class="layout">
         <div>
@@ -30,6 +30,46 @@ import AnchorViewCaseStudy from '@/components/AnchorViewCaseStudy.vue'
 import ImageSet from '@/components/ImageSet.vue'
 
 export default {
+  data() {
+    return {
+      scrollTop: 0,
+      popIt: false,
+      popAt: [1280, 690]
+    }
+  },
+  computed: {
+    elOffsetTop() {
+      return this.$el.getBoundingClientRect().top + window.scrollY
+    }
+  },
+  methods: {
+    cardPop() {
+      if (
+        window.innerWidth < this.popAt[0] ||
+        window.innerHeight < this.popAt[1]
+      ) {
+        return
+      }
+
+      this.scrollTop = document.documentElement.scrollTop
+
+      if (this.scrollTop + window.innerHeight / 2 >= this.elOffsetTop) {
+        this.popIt = true
+      }
+      if (this.scrollTop + window.innerHeight / 2 <= this.elOffsetTop) {
+        this.popIt = false
+      }
+    }
+  },
+  mounted() {
+    this.scrollTop = document.documentElement.scrollTop
+
+    this.cardPop()
+
+    window.addEventListener('scroll', () => {
+      this.cardPop()
+    })
+  },
   components: {
     AnchorViewCaseStudy,
     ImageSet
