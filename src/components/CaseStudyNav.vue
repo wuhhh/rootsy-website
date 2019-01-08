d836d29fe9d943266330b719e82bb0d7755a845a
Huw Roberts MBP <huwroberts@Huws-MBP.lan>
2019-01-08 20:47:15 +0000

    Moved CaseStudy nav to component

---
+++ src/components/CaseStudyNav.vue
@@ -0,0 +1,95 @@
+<template>
+  <nav class="work-prev-next">
+    <div class="layout">
+      <a href="#" class="work-prev-next--prev">
+        <span href="#" rel="prev">Prev</span>
+      </a>
+      <a href="#" class="work-prev-next--next">
+        <span href="#" rel="next">Next</span>
+      </a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+@import '@/sass/_settings.scss';
+
+nav.work-prev-next {
+  margin-top: 8em;
+
+  .layout {
+    display: flex;
+
+    & > a {
+      width: 50%;
+      text-decoration: none;
+    }
+  }
+
+  .work-prev-next--prev,
+  .work-prev-next--next {
+    padding: 4em 3em;
+
+    span {
+      display: inline-block;
+      font-family: 'CanelaDeck';
+      /*font-size: 4em;*/
+      line-height: normal;
+      /*width: 50%;*/
+      font-size: 2.5em;
+      width: 100%;
+      border-bottom: 2px solid;
+      transition: width 350ms ease;
+
+      @media screen and (min-width: 400px) {
+        width: 90%;
+      }
+
+      @media screen and (min-width: 600px) {
+        font-size: 3em;
+        width: 70%;
+      }
+
+      @media screen and (min-width: 800px) {
+        width: 55%;
+      }
+
+      @media screen and (min-width: 1000px) {
+        width: 45%;
+      }
+    }
+
+    &:hover {
+      span {
+        width: 100%;
+      }
+    }
+  }
+
+  .work-prev-next--prev {
+    background-color: lighten($c-pink, 10%);
+    display: flex;
+    justify-content: flex-end;
+    text-align: right;
+
+    span {
+      color: $c-blue;
+      border-bottom-color: $c-blue;
+    }
+  }
+
+  .work-prev-next--next {
+    background-color: lighten($c-blue, 5%);
+    text-align: left;
+
+    span {
+      color: lighten($c-pink, 10%);
+      border-bottom-color: lighten($c-pink, 10%);
+    }
+  }
+}
+</style>
