9258b4de96f89a6f0fc4cc50caa3422ca4eb1791
Huw Roberts MBP <huwroberts@Huws-MBP.lan>
2019-01-08 23:26:49 +0000

    Lodash added

--- src/main.js
+++ src/main.js
@@ -4,8 +4,13 @@ import router from './router'
 import store from './store'
 import BaseMarkdown from '@/components/BaseMarkdown.vue'
 
+// Register lodash core
+var _ = require('lodash')
+
+// Global component
 Vue.component('BaseMarkdown', BaseMarkdown)
 
+// Don't show production tips
 Vue.config.productionTip = false
 
 // https://github.com/quasarframework/quasar/issues/1466#issuecomment-414066098
