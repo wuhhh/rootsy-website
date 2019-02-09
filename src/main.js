9af4859546a0afe7843dd84910385ee52d2ee38c
Huw Roberts MBP <huwroberts@Huws-MBP.lan>
2019-02-09 15:37:33 +0000

    Added comments

--- src/main.js
+++ src/main.js
@@ -4,26 +4,37 @@ import router from './router'
 import store from './store'
 import BaseMarkdown from '@/components/BaseMarkdown.vue'
 
-// Global component
+// Register lobal components
 Vue.component('BaseMarkdown', BaseMarkdown)
 
 // Don't show production tips
 Vue.config.productionTip = false
 
-// Zap orphans / widows
+/**
+ * Prevent widowing via mixin
+ */
 import unorphan from 'unorphan'
 
 Vue.mixin({
   mounted: function() {
-    unorphan(document.querySelectorAll('p'))
+    unorphan(document.querySelectorAll('h1, p'))
   }
 })
 
-// https://github.com/quasarframework/quasar/issues/1466#issuecomment-414066098
+/**
+ * It looks like modern browsers set the scroll to savedPosition immediately
+ * when you navigate using the browser buttons, so returning a promise from
+ * scrollBehavior does not fix the flickering issue entirely.
+ *
+ * https://github.com/quasarframework/quasar/issues/1466#issuecomment-414066098
+ *
+ * In order to prevent the browser's default behavior, do this:
+ */
 if ('scrollRestoration' in history) {
   history.scrollRestoration = 'manual'
 }
 
+// Create the root vue instance
 new Vue({
   router,
   store,
