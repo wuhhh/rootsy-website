6c4d77a39e283224c525b6df3db9a77dca599b04
Huw Roberts iMac <huwroberts@Huws-iMac.lan>
2019-06-17 18:58:03 +0100

    ScrolledIntoView IntersectionObserver component added
    Replaces old clientBoundingRect method of showing case study card ‘pop’ .
    
    Not supported by Safari which was, in any case, not showing the pop transition.

--- src/components/CaseStudyCard.vue
+++ src/components/CaseStudyCard.vue
@@ -1,91 +1,57 @@
 <template>
-  <div
-    :style="{ backgroundColor: caseStudy.acf.background_colour }"
-    :class="[{hello: popIt}, {is_dark: caseStudy.acf.dark_mode}]"
-    class="fit-height featured-work--item item"
-  >
-    <div class="pad">
-      <div class="layout">
-        <div>
-          <div class="item--words">
-            <div class="item--title">{{ caseStudy.title.rendered }}</div>
-            <div class="item--description">
-              <div v-html="caseStudy.acf.excerpt"/>
-              <p>
-                <AnchorViewCaseStudy :isDark="caseStudy.acf.dark_mode" :slug="caseStudy.slug"/>
-              </p>
+  <div>
+    <ScrolledIntoView>
+      <template v-slot:default="slotProps">
+        <div
+          :style="{ backgroundColor: caseStudy.acf.background_colour }"
+          :class="[
+            { hello: slotProps.inRange },
+            { is_dark: caseStudy.acf.dark_mode }
+          ]"
+          class="fit-height featured-work--item item"
+        >
+          <div class="pad">
+            <div class="layout">
+              <div>
+                <div class="item--words">
+                  <div class="item--title">{{ caseStudy.title.rendered }}</div>
+                  <div class="item--description">
+                    <div v-html="caseStudy.acf.excerpt" />
+                    <p>
+                      <AnchorViewCaseStudy
+                        :isDark="caseStudy.acf.dark_mode"
+                        :slug="caseStudy.slug"
+                      />
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div class="item--image">
+                <ImageSet
+                  :width="caseStudy.acf.primary_image.width"
+                  :height="caseStudy.acf.primary_image.height"
+                  :sizes="caseStudy.acf.primary_image.sizes"
+                  :alt="caseStudy.acf.primary_image.alt"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div class="item--image">
-          <ImageSet
-            :width="caseStudy.acf.primary_image.width"
-            :height="caseStudy.acf.primary_image.height"
-            :sizes="caseStudy.acf.primary_image.sizes"
-            :alt="caseStudy.acf.primary_image.alt"
-          />
-        </div>
-      </div>
-    </div>
+      </template>
+    </ScrolledIntoView>
   </div>
 </template>
 
 <script>
 import AnchorViewCaseStudy from '@/components/AnchorViewCaseStudy.vue'
 import ImageSet from '@/components/ImageSet.vue'
+import ScrolledIntoView from '@/components/ScrolledIntoView.vue'
 
 export default {
-  data() {
-    return {
-      scrollTop: 0,
-      popIt: false,
-      popAt: [1280, 690]
-    }
-  },
-  computed: {
-    elOffsetTop() {
-      return this.$el.getBoundingClientRect().top + window.scrollY
-    }
-  },
-  methods: {
-    cardPop() {
-      // Don't pop if window size under popAt[0] x popAt[1]
-      if (
-        window.innerWidth < this.popAt[0] ||
-        window.innerHeight < this.popAt[1]
-      ) {
-        return
-      }
-
-      this.scrollTop = document.documentElement.scrollTop
-
-      if (this.scrollTop + window.innerHeight / 2 >= this.elOffsetTop) {
-        this.popIt = true
-      }
-      if (this.scrollTop + window.innerHeight / 2 <= this.elOffsetTop) {
-        this.popIt = false
-      }
-    }
-  },
-  mounted() {
-    /**
-     * Pop cards
-     */
-    this.scrollTop = document.documentElement.scrollTop
-
-    this.cardPop()
-
-    window.addEventListener(
-      'scroll',
-      () => {
-        this.cardPop()
-      },
-      { passive: true }
-    )
-  },
   components: {
     AnchorViewCaseStudy,
-    ImageSet
+    ImageSet,
+    ScrolledIntoView
   },
   props: {
     caseStudy: Object
@@ -93,7 +59,7 @@ export default {
 }
 </script>
 
-<style  lang="scss">
+<style lang="scss">
 @import '@/sass/_settings.scss';
 
 .item.is_dark {
---
+++ src/components/ScrolledIntoView.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <slot :inRange="inRange"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ScrolledIntoView',
+  data() {
+    return {
+      inRange: false,
+      observer: null
+    }
+  },
+  props: {
+    // Pad horizontal centre up and down
+    midPad: {
+      type: Number,
+      required: false,
+      default: 600
+    },
+    // Offset midPad
+    offset: {
+      type: Number,
+      required: false,
+      default: 0
+    },
+    // Use position of parent El instead of this.$el
+    useParent: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    observerOptions: {
+      type: Object,
+      required: false,
+      default: () => {
+        return {
+          root: null,
+          rootMargin: '0px',
+          threshold: 1.0
+        }
+      }
+    }
+  },
+  computed: {
+    pad() {
+      return this.midPad + this.offset
+    }
+  },
+  mounted() {
+    // https://github.com/w3c/IntersectionObserver/blob/master/polyfill/intersection-observer.js
+    // Check if  IntersectionObserver and IntersectionObserverEntry
+    // features are natively supported.
+    if (
+      'IntersectionObserver' in window &&
+      'IntersectionObserverEntry' in window &&
+      'intersectionRatio' in window.IntersectionObserverEntry.prototype
+    ) {
+      // Minimal polyfill for Edge 15's lack of `isIntersecting`
+      // See: https://github.com/w3c/IntersectionObserver/issues/211
+      if (!('isIntersecting' in window.IntersectionObserverEntry.prototype)) {
+        Object.defineProperty(
+          window.IntersectionObserverEntry.prototype,
+          'isIntersecting',
+          {
+            get: function() {
+              return this.intersectionRatio > 0
+            }
+          }
+        )
+      }
+
+      // Observer callback
+      const observerCallback = entries => {
+        if (entries[0].isIntersecting) this.inRange = true
+      }
+
+      // Create the observer
+      this.observer = new IntersectionObserver(
+        observerCallback,
+        this.observerOptions
+      )
+
+      // Watch
+      let target = this.useParent ? this.$parent.$el : this.$el
+      this.observer.observe(target)
+    }
+  }
+}
+</script>
+
+<style></style>
