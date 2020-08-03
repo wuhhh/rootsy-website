6abfed443de9370ca9907f9d80096c71a8ac2fda
Huw Roberts <huwrroberts@gmail.com>
2020-08-03 22:53:47 +0100

    Config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true,
   },
   extends: [
     "plugin:vue/recommended",
     "eslint:recommended",
     "prettier/vue",
-    "plugin:prettier/recommended"
+    "plugin:prettier/recommended",
   ],
   rules: {
     "vue/component-name-in-template-casing": ["error", "PascalCase"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/no-v-html": "off"
+    "prettier/prettier:": "off",
   },
   globals: {
-    $nuxt: true
+    $nuxt: true,
   },
   parserOptions: {
-    parser: "babel-eslint"
-  }
-};
+    parser: "babel-eslint",
+  },
+}
