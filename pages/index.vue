bf510e8799fe7eb6a3b4befc843433e6fd288d0c
Huw Roberts <huwrroberts@gmail.com>
2020-08-04 20:30:50 +0100

    Just header & footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
     <Header />
-    <CaseStudies v-if="list" />
-    <About />
-    <Clients />
+
     <Footer />
   </div>
 </template>
@@ -12,18 +10,14 @@
 import { mapState } from "vuex";
 
 import Header from "@/components/Header.vue";
-import CaseStudies from "@/components/CaseStudies.vue";
-import About from "@/components/About.vue";
-import Clients from "@/components/Clients.vue";
+
 import Footer from "@/components/Footer.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
-    CaseStudies,
-    About,
-    Clients,
+
     Footer
   },
   head: {
