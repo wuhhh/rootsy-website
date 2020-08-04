9daeb975adf569f5035410ffb252411bb179a686
Huw Roberts <huwrroberts@gmail.com>
2020-08-04 20:42:17 +0100

    Everything except siney / about

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <Header />
-
+    <CaseStudies v-if="list" />
+    <Clients />
     <Footer />
   </div>
 </template>
@@ -10,14 +11,16 @@
 import { mapState } from "vuex";
 
 import Header from "@/components/Header.vue";
-
+import CaseStudies from "@/components/CaseStudies.vue";
+import Clients from "@/components/Clients.vue";
 import Footer from "@/components/Footer.vue";
 
 export default {
   name: "Home",
   components: {
     Header,
-
+    CaseStudies,
+    Clients,
     Footer
   },
   head: {
